b74ae87e4d566e094411c038c6cb8cb4baafc736
gouriPARMAR <parmargouri07@gmail.com>
2025-05-06 07:38:29 +0530

    Add files via upload

---
+++ assignment5/PROGRAMS/61.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main(){
+    cout<<"Name"<<setw(15)<<"Age"<<setw(15)<<"Phone No."<<endl  
+        <<"Gouri"<<setw(15)<<"18"<<setw(15)<<"9475532574"<<endl
+        <<"Mahi"<<setw(14)<<"14"<<setw(15)<<"6474362433"<<endl;
+    return 0;
+}
